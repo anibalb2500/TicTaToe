58ee78298812317b472ca7b6ae238132ac8e352b
Anibal Bojorquez <bojorquezanibal@gmail.com>
2024-10-13 09:31:00 -0700

    added player assignments

--- index.js
+++ index.js
@@ -2,23 +2,63 @@ import { Server } from "socket.io";
 
 const io = new Server({ /* options */ });
 
-const messages = [];
+// Define the Player enum
+const Player = {
+    X: 'X',
+    O: 'O'
+  };
+  
+// Update the messages variable to be an array of arrays of nullable Player
+// Initially:
+//     messages[0] = [[null, null, null]], messages[1] = [[null, null, null]], and messages[2] = [[null, null, null]]
+const coordinates = Array(3).fill().map(() => [null, null, null]);
+
+// Track the users
+let users = [];
 
 io.on("connection", (socket) => {
-  console.log("user connected")
 
-  socket.on("newMessage", (message) => {
-    console.log("new message", message);
-    // Store the new message
-    messages.push(message);
+    console.log("New client connected");
 
-    // Broadcast the new message to all connected clients
-    io.emit("newMessage", message);
+    // Check if the room is full
+    if (users.length >= 2) {
+        console.log("Room is full");
+        socket.emit("roomFull");
+        socket.disconnect();
+        return;
+    }
+
+    // Assign player
+    const player = users.length === 0 ? Player.X : Player.O;
+    users.push({ id: socket.id, player });
+
+    // Emit the initial state of the game to the client
+    socket.emit("initialState", { 
+        coordinates,
+        player
+     } );
+
+  socket.on("newMove", (message) => {
+    const { player, x, y } = message;
+
+    if (player === Player.X || player === Player.O && Number.isInteger(x) && Number.isInteger(y)) {
+        // Process the message
+        console.log(`Received message: ${message}`);
+        // At coordinates[x][y] set the value to player - Maybe add a check to see if the coordinates are valid
+        coordinates[x][y] = player;
+        // Emit the updated coordinates to all clients - Maybe the DS update was successful
+        io.emit("successfullyAdded", successfullyAdded);
+      } else {
+        console.error("Invalid message type");
+      }
   });
 });
 
-io.on("disconnect", (socket) => {});
+io.on("disconnect", (socket) => {
+    console.log("Client disconnected");
+    users = users.filter(user => user.id !== socket.id);
+});
 
-const port = process.env.PORT || 3000;
+const port = 3000;//process.env.PORT || 3000;
 
 io.listen(port);

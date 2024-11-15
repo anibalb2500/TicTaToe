157510154b62f807da39def743ec7bb01df6e675
Anibal Bojorquez <bojorquezanibal@gmail.com>
2024-11-15 11:29:22 -0700

    added event handling for when opponent leaves game room

--- android/app/src/main/java/com/example/myapplication/states/BoardState.kt
+++ android/app/src/main/java/com/example/myapplication/states/BoardState.kt
@@ -5,8 +5,10 @@ import com.example.myapplication.models.Player
 sealed class  BoardState {
     object Idle : BoardState()
     object WaitingForOtherPlayer : BoardState()
+    object OpponentLeft : BoardState()
     data class Playing(
         val yourPlayer: Player,
         val currentPlayer: Player
     ) : BoardState()
+
 }
